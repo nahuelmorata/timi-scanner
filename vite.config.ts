4420645cded932c0f3d88d5f2f9f83a59381a7b2
Nahuel <nahuelmorata@gmail.com>
2025-11-12 20:43:14 -0300

    feat: update types

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,26 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, PluginOption } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => ({
-  plugins: [vue(), vueJsx(), mode !== 'production' && vueDevTools(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(async ({ mode }) => {
+  const plugins: PluginOption[] = [vue(), vueJsx(), tailwindcss()]
+
+  if (mode !== 'production') {
+    const mod = await import('vite-plugin-vue-devtools')
+    const vueDevTools = mod.default
+    if (vueDevTools) {
+      plugins.push(vueDevTools() as PluginOption)
+    }
+  }
+
+  return {
+    plugins,
+    resolve: {
+      alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) },
     },
-  },
-}))
+  }
+})
