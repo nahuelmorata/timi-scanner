caad5fcc1e9ba56d3f1557e4017b97b326e10039
Nahuel <nahuelmorata@gmail.com>
2025-11-24 23:45:20 -0300

    feat: cambio url para scanner

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,7 @@ export default defineConfig(async ({ mode }) => {
   }
 
   return {
+    base: '/scanner',
     plugins,
     resolve: {
       alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) },
